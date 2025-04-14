fac634d68463d7e7137b3eaae809b6e56a46796e
Young4424 <rladydgnj@gmail.com>
2025-04-14 14:22:33 +0900

    fix syntax error

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_connection():
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
-        database=DB_NAME
+        database=DB_NAME,
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
