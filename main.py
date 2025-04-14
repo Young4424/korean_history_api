0ce4671aecd870ea8915eb9e7233f135ddef18e4
Young4424 <rladydgnj@gmail.com>
2025-04-14 16:15:02 +0900

    fix code

--- main.py
+++ main.py
@@ -491,5 +491,5 @@ async def upload_tts_audio(file: UploadFile = File(...)):
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
