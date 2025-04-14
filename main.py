7516fd95e57209215653ed56a1101b0e1a4aa33b
Young4424 <rladydgnj@gmail.com>
2025-04-14 14:32:24 +0900

    fixed main.py

--- main.py
+++ main.py
@@ -464,5 +464,5 @@ async def save_note_analysis_complete(
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"분석 결과 저장 실패: {str(e)}")
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
